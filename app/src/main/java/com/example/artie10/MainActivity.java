f2d7497af21ec363976c5d04e9b360c28347bb0f
Sarper Turan <sarper.turan@ug.bilkent.edu.tr>
2020-04-24 12:00:43 +0300

    Categories Screen - Beginning

--- app/src/main/java/com/example/artie10/MainActivity.java
+++ app/src/main/java/com/example/artie10/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
         switch (item.getItemId()) {
             case R.id.newSession:
                 Toast.makeText(this, "Item 1 clicked", Toast.LENGTH_SHORT).show();
+                openCategories();
                 return true;
             case R.id.joinSession:
                 Toast.makeText(this, "Item 2 clicked", Toast.LENGTH_SHORT).show();
@@ -49,6 +50,11 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
 
     public void openActivity2(){
         Intent intent = new Intent(this, SecondScreen.class);
+        startActivity(intent);
+    }
+
+    public void openCategories(){
+        Intent intent = new Intent(this, Categories.class);
         startActivity(intent);
     }
 }
