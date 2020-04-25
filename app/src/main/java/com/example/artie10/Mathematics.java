950b9340bd4d46909fc84742396e92612f5809e2
OykuErhan <oyku.erhan@ug.bilkent.edu.tr>
2020-04-25 16:07:45 +0300

    Merge branch 'master' of C:\Users\Ozgu\AndroidStudioProjects\ARtIE1.0 with conflicts.

---
+++ app/src/main/java/com/example/artie10/Mathematics.java
@@ -0,0 +1,16 @@
+package com.example.artie10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Mathematics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mathematics);
+    }
+
+}
