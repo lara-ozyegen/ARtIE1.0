ec2146839c5300eba7b3657ebdb9b02e5d0a1557
Sarper Turan <sarper.turan@ug.bilkent.edu.tr>
2020-04-25 14:18:11 +0300

    categories with action bar

--- app/src/main/java/com/example/artie10/Categories.java
+++ app/src/main/java/com/example/artie10/Categories.java
@@ -2,6 +2,10 @@ package com.example.artie10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.drm.DrmStore;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 public class Categories extends AppCompatActivity {
@@ -10,5 +14,8 @@ public class Categories extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories3);
+
+        getSupportActionBar().setBackgroundDrawable(
+                new ColorDrawable(Color.parseColor("#BE40FF")));
     }
 }
