96a27637d2468b0faf2148ae4b38200c1e0caba3
Sarper Turan <sarper.turan@ug.bilkent.edu.tr>
2020-04-25 15:45:12 +0300

    added listener to biology

---
+++ app/src/main/java/com/example/artie10/Biology.java
@@ -0,0 +1,16 @@
+package com.example.artie10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+public class Biology extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_biology);
+    }
+}
--- app/src/main/java/com/example/artie10/Categories.java
+++ app/src/main/java/com/example/artie10/Categories.java
@@ -3,6 +3,7 @@ package com.example.artie10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.drm.DrmStore;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -21,62 +22,15 @@ public class Categories extends AppCompatActivity {
                 new ColorDrawable(Color.parseColor("#BE40FF")));
 
         //Adding onClickListeners for all of the categories
-        ImageView addModel = (ImageView) findViewById(R.id.addModel);
-        addModel.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                //opens downloads page --- how ?
-            }
-        });
-
-        ImageView myModels = (ImageView) findViewById(R.id.myModels);
-        myModels.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                openMyModels();
-            }
-        });
-
-        ImageView maths = (ImageView) findViewById(R.id.maths);
-        maths.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                openMaths();
-            }
-        });
-
-        ImageView chem = (ImageView) findViewById(R.id.chemistry);
-        chem.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                openChemistry();
-            }
-        });
-
         ImageView bio = (ImageView) findViewById(R.id.bio);
         bio.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 openBiology();
             }
         });
-
-        ImageView space = (ImageView) findViewById(R.id.space);
-        space.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                openSpace();
-            }
-        });
-
-        public void openBiology(){
-
-        }
-        public void openMaths(){
-
-        }
-        public void openMyModels(){
-
-        }
-        public void openSpace(){
-
-        }
-        public void openChemistry(){
-
-        }
+    }
+    public void openBiology(){
+        Intent intent = new Intent(this, Biology.class);
+        startActivity(intent);
     }
 }
