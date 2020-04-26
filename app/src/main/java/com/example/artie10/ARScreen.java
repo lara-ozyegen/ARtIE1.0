8b66ef3a525e02e9004753c652148072091cc180
Sarper Turan <sarper.turan@ug.bilkent.edu.tr>
2020-04-26 15:28:56 +0300

    ARScreen

---
+++ app/src/main/java/com/example/artie10/ARScreen.java
@@ -0,0 +1,47 @@
+package com.example.artie10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+public class ARScreen extends AppCompatActivity {
+
+    private ArFragment arFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        arFragment = (ArFragment)getSupportFragmentManager().findFragmentById(R.id.arFragment);
+        arFragment.setOnTapArPlaneListener((hitResult,plane,motionEvent) -> {
+            Anchor anchor = hitResult.createAnchor();
+            ModelRenderable.builder()
+                    .setSource(this, Uri.parse("ArcticFox_Posed.sfb"))
+                    .build()
+                    .thenAccept(modelRenderable -> addModelToScene(anchor, modelRenderable))
+                    .exceptionally(throwable -> {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                        builder.setMessage(throwable.getMessage())
+                                .show();
+                        return null;
+                    });
+        });
+    }
+
+    private void addModelToScene(Anchor anchor, ModelRenderable modelRenderable) {
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        TransformableNode transformableNode = new TransformableNode(arFragment.getTransformationSystem());
+        transformableNode.setParent(anchorNode);
+        transformableNode.setRenderable(modelRenderable);
+        arFragment.getArSceneView().getScene().addChild(anchorNode);
+        transformableNode.select();
+    }
+}
--- app/src/main/java/com/example/artie10/Preview.java
+++ app/src/main/java/com/example/artie10/Preview.java
@@ -3,8 +3,11 @@ package com.example.artie10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
 
 public class Preview extends Activity {
 
@@ -19,6 +22,18 @@ public class Preview extends Activity {
         int width = dm. widthPixels;
         int height = dm.heightPixels;
         getWindow().setLayout((int) (width*.8), (int) (height*.6));
+
+        Button b = (Button) findViewById( R.id.ar_button);
+        b.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick( View v) {
+                openAR();
+            }
+        } );
+    }
+    public void openAR(){
+        Intent intent = new Intent(this, ARScreen.class);
+        startActivity(intent);
     }
 
 
