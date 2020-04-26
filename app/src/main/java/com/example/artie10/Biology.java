b3c7075cd6e844eddbe9a501907c6885cec74d0c
EnisOzer <63506361+EnisOzer@users.noreply.github.com>
2020-04-26 14:32:00 +0300

    preview screen done

--- app/src/main/java/com/example/artie10/Biology.java
+++ app/src/main/java/com/example/artie10/Biology.java
@@ -3,9 +3,12 @@ package com.example.artie10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Biology extends Activity {
 
@@ -13,5 +16,15 @@ public class Biology extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_biology);
+        Button b = (Button) findViewById( R.id.heart_button);
+        b.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick( View v) {
+                openPreview();
+            }
+        } );
+    }
+    public void openPreview(){
+        Intent intent = new Intent(this , Preview.class );
     }
 }
--- app/src/main/java/com/example/artie10/Preview.java
+++ app/src/main/java/com/example/artie10/Preview.java
@@ -2,13 +2,24 @@ package com.example.artie10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
-public class Preview extends AppCompatActivity {
+public class Preview extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preview);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm. widthPixels;
+        int height = dm.heightPixels;
+        getWindow().setLayout((int) (width*.8), (int) (height*.6));
     }
+
+
 }
