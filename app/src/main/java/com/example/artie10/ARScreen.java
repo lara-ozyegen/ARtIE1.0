b4a6614e1166e76284f4da55fcb92dfab60302cc
Sarper Turan <sarper.turan@ug.bilkent.edu.tr>
2020-04-27 17:38:39 +0300

    Heart changed

--- app/src/main/java/com/example/artie10/ARScreen.java
+++ app/src/main/java/com/example/artie10/ARScreen.java
@@ -24,7 +24,7 @@ public class ARScreen extends AppCompatActivity {
         arFragment.setOnTapArPlaneListener((hitResult,plane,motionEvent) -> {
             Anchor anchor = hitResult.createAnchor();
             ModelRenderable.builder()
-                    .setSource(this, Uri.parse("Heart_.sfb"))
+                    .setSource(this, Uri.parse("1410 Heart.sfb"))
                     .build()
                     .thenAccept(modelRenderable -> addModelToScene(anchor, modelRenderable))
                     .exceptionally(throwable -> {
