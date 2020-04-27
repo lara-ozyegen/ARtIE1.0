1743cf78e820407c5b364d4470056395720cee7b
OykuErhan <oyku.erhan@ug.bilkent.edu.tr>
2020-04-27 16:14:12 +0300

    add models open folder

--- app/src/main/java/com/example/artie10/Categories.java
+++ app/src/main/java/com/example/artie10/Categories.java
@@ -56,6 +56,7 @@ public class Categories extends AppCompatActivity {
         startActivityForResult(intent, REQUEST_CODE);
     }
 
+
     protected void onActivityResult( int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
         if( requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK){
