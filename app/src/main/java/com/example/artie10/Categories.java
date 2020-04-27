55a85da2ec10715a695dac3542f578da5b0291cd
OykuErhan <oyku.erhan@ug.bilkent.edu.tr>
2020-04-27 16:09:39 +0300

    add models open folder

--- app/src/main/java/com/example/artie10/Categories.java
+++ app/src/main/java/com/example/artie10/Categories.java
@@ -8,13 +8,17 @@ import android.content.Intent;
 import android.drm.DrmStore;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Categories extends AppCompatActivity {
     private Button bio;
+    private Button add;
+    private static final int REQUEST_CODE = 45;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +35,35 @@ public class Categories extends AppCompatActivity {
                 openBiology();
             }
         });
+
+        add = (Button) findViewById(R.id.addButton);
+        add.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                addModel();
+            }
+        });
     }
     public void openBiology(){
         Intent intent = new Intent(Categories.this, Biology.class);
         startActivity(intent);
+    }
+
+
+    private void addModel(){
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    protected void onActivityResult( int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if( requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK){
+            if( data != null){
+                Uri uri = data.getData();
+                Toast.makeText( this, "Uri :" +uri, Toast.LENGTH_LONG ).show();
+                //Toast.makeText( this, "Path :" + uri.getPath(), Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
