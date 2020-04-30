e1c14ba0e172ac54c4a40fb1f50ca94ccbb2c635
laraOzygn <lara.ozyegen@ug.bilkent.edu.tr>
2020-05-01 01:05:38 +0300

    Screenshot feature

--- app/src/main/java/com/example/artie10/ARScreen.java
+++ app/src/main/java/com/example/artie10/ARScreen.java
@@ -1,10 +1,16 @@
 package com.example.artie10;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlertDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.google.ar.core.Anchor;
 import com.google.ar.sceneform.AnchorNode;
@@ -12,16 +18,25 @@ import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Calendar;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+
 public class ARScreen extends AppCompatActivity {
 
     private ArFragment arFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a_r_screen);
-        arFragment = (ArFragment)getSupportFragmentManager().findFragmentById(R.id.arFragment);
-        arFragment.setOnTapArPlaneListener((hitResult,plane,motionEvent) -> {
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
+        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
             Anchor anchor = hitResult.createAnchor();
             ModelRenderable.builder()
                     .setSource(this, Uri.parse("1410 Heart.sfb"))
@@ -34,7 +49,14 @@ public class ARScreen extends AppCompatActivity {
                         return null;
                     });
         });
-    }
+
+        ActivityCompat.requestPermissions(this, new String[] {
+            WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, PERMISSION_GRANTED);
+
+        //otherwise app crashes
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+    }//end of onCreate
 
     private void addModelToScene(Anchor anchor, ModelRenderable modelRenderable) {
         AnchorNode anchorNode = new AnchorNode(anchor);
@@ -44,4 +66,40 @@ public class ARScreen extends AppCompatActivity {
         arFragment.getArSceneView().getScene().addChild(anchorNode);
         transformableNode.select();
     }
+
+
+    public void ScreenshotButton( View view) {
+        //take screenshot
+        View view1 = getWindow().getDecorView().getRootView();
+        //View view1 = findViewById(R.id.relativeLayout2);
+        view1.setDrawingCacheEnabled(true);
+
+        //create bitmap to draw the screenshot
+        Bitmap bitmap = Bitmap.createBitmap(view1.getDrawingCache());
+        view1.setDrawingCacheEnabled(false);
+
+        //create file
+        String filePath = Environment.getExternalStorageDirectory()+"/Download/"+ Calendar.getInstance().getTime().toString() + ".jpg";
+        File fileScreenshot = new File(filePath);
+
+        FileOutputStream fileOutputStream = null;
+
+        try {
+            fileOutputStream = new FileOutputStream(fileScreenshot);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        Uri uri = Uri.fromFile(fileScreenshot);
+        intent.setDataAndType(uri,"image/.jpeg");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        this.startActivity(intent);
+    }
+
+
 }
