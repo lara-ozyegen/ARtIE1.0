b37ba3eff9624e5c4375152b0ccaa2f259fab9e1
OykuErhan <oyku.erhan@ug.bilkent.edu.tr>
2020-05-02 12:06:07 +0300

    Merge branch 'master' of C:\Users\Ozgu\AndroidStudioProjects\ARtIE1.0 with conflicts.

--- app/src/main/java/com/example/artie10/ARScreen.java
+++ app/src/main/java/com/example/artie10/ARScreen.java
@@ -1,21 +1,41 @@
 package com.example.artie10;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.MediaRecorder;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.StrictMode;
+import android.util.DisplayMetrics;
+import android.util.SparseIntArray;
+import android.view.Surface;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+import android.widget.VideoView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.ar.core.Anchor;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.rendering.ModelRenderable;
@@ -24,7 +44,10 @@ import com.google.ar.sceneform.ux.TransformableNode;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
@@ -34,9 +57,30 @@ public class ARScreen extends AppCompatActivity {
 
     private ArFragment arFragment;
     private ImageView pencil;
-    private PaintView paintView;
+    private VideoView videoView;
+    private ToggleButton toggleButton;
+    private RelativeLayout relativeLayout2;
+    private String videoURI = "";
+    private static final int REQUEST_CODE = 1000;
+    private static final int REQUEST_PERMISSION = 1001;
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
 
+    private MediaProjectionManager mediaProjectionManager;
+    private MediaProjection mediaProjection;
+    private MediaProjectionCallBack mediaProjectionCallBack;
+    private MediaRecorder mediaRecorder;
 
+    private VirtualDisplay virtualDisplay;
+    private int mScreenDensity;
+    private static final int DISPLAY_WIDTH = 720;
+    private static final int DISPLAY_HEIGHT = 1280;
+
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0,90);
+        ORIENTATIONS.append(Surface.ROTATION_90,0);
+        ORIENTATIONS.append(Surface.ROTATION_180,270);
+        ORIENTATIONS.append(Surface.ROTATION_270,180);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,7 +89,7 @@ public class ARScreen extends AppCompatActivity {
         arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
             Anchor anchor = hitResult.createAnchor();
             ModelRenderable.builder()
-                    .setSource(this, Uri.parse("Human Heart.sfb"))
+                    .setSource(this, Uri.parse("1410 Heart.sfb"))
                     .build()
                     .thenAccept(modelRenderable -> addModelToScene(anchor, modelRenderable))
                     .exceptionally(throwable -> {
@@ -60,6 +104,7 @@ public class ARScreen extends AppCompatActivity {
         pencil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                openPaint();
             }
         });
 
@@ -70,6 +115,57 @@ public class ARScreen extends AppCompatActivity {
         //otherwise app crashes
         StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
         StrictMode.setVmPolicy(builder.build());
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        mScreenDensity = metrics.densityDpi;
+
+        mediaRecorder = new MediaRecorder();
+        mediaProjectionManager = (MediaProjectionManager)getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+
+        //view
+        videoView = (VideoView)findViewById(R.id.videoView);
+        toggleButton = (ToggleButton)findViewById(R.id.toggleButton);
+        //relativeLayout2 = (RelativeLayout)findViewById(R.id.relativeLayout2);
+
+        //event
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ContextCompat.checkSelfPermission(ARScreen.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        + ContextCompat.checkSelfPermission(ARScreen.this, Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED)
+                {
+                    if(ActivityCompat.shouldShowRequestPermissionRationale(ARScreen.this, WRITE_EXTERNAL_STORAGE) ||
+                            ActivityCompat.shouldShowRequestPermissionRationale(ARScreen.this, Manifest.permission.RECORD_AUDIO))
+                    {
+                        toggleButton.setChecked(false);
+                        Snackbar.make(relativeLayout2, "Permissions", Snackbar.LENGTH_INDEFINITE)
+                                .setAction("ENABLE", new View.OnClickListener() {
+                       @Override
+                       public void onClick(View v) {
+                        ActivityCompat.requestPermissions(ARScreen.this,
+                                new String[]{
+                                        WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO},
+                                REQUEST_PERMISSION);
+                       }
+
+                    }).show();
+
+                     }
+                         else{
+                                 ActivityCompat.requestPermissions(ARScreen.this,
+                                    new String[]{
+                                            WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO},
+                                    REQUEST_PERMISSION);
+                            }
+                }
+                else{
+                    toggleScreenShare(v);
+                }
+             }
+
+        });
     }//end of onCreate
 
     private void addModelToScene(Anchor anchor, ModelRenderable modelRenderable) {
@@ -102,14 +198,13 @@ public class ARScreen extends AppCompatActivity {
         ////Bitmap bitmap = Bitmap.createBitmap(screenView.getDrawingCache());
         ////screenView.setDrawingCacheEnabled(false);
         Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        view1.buildDrawingCache(false);
-        Canvas canvas = new Canvas( bitmap);
-        Drawable bgDrawable = view1.getBackground();
-        //if (bgDrawable != null)
+        Canvas canvas = new Canvas(bitmap);
+        Drawable bgDrawable = view.getBackground();
+        if (bgDrawable != null)
             bgDrawable.draw(canvas);
-        //else
-            canvas.drawColor(Color.TRANSPARENT);
-        view1.draw( canvas);
+        else
+            canvas.drawColor(Color.WHITE);
+        view.draw(canvas);
 
         //create file
         String filePath = Environment.getExternalStorageDirectory()+"/Download/"+ Calendar.getInstance().getTime().toString() + ".jpg";
@@ -134,6 +229,151 @@ public class ARScreen extends AppCompatActivity {
         this.startActivity(intent);
     }
 
+    public void openPaint(){
+        Intent intent = new Intent(this,PaintActivity.class);
+        startActivity(intent);
+    }
 
 
+    private class MediaProjectionCallBack extends MediaProjection.Callback {
+        @Override
+        public void onStop() {
+            if(toggleButton.isChecked()){
+                toggleButton.setChecked(false);
+                mediaRecorder.stop();
+                mediaRecorder.reset();
+            }
+            mediaProjection = null;
+            stopRecordScreen();
+            super.onStop();
+        }
+    }
+
+    private void stopRecordScreen() {
+        if(virtualDisplay == null)
+            return;
+        virtualDisplay.release();
+        destroyMediaProjection();
+    }
+
+    private void destroyMediaProjection() {
+        if(mediaProjection != null){
+            mediaProjection.unregisterCallback(mediaProjectionCallBack);
+            mediaProjection.stop();
+            mediaProjection = null;
+        }
+
+    }
+
+    private void toggleScreenShare(View v){
+        if(((ToggleButton)v).isChecked())
+        {
+            initRecorder();
+            recordScreen();
+        }
+        else{
+            mediaRecorder.stop();
+            mediaRecorder.reset();
+            stopRecordScreen();
+
+            videoView.setVisibility(View.VISIBLE);
+            videoView.setVideoURI(Uri.parse(videoURI));
+            videoView.start();
+        }
+    }
+
+    private void initRecorder(){
+        try{
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+
+            videoURI = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+                    + new StringBuilder("/EDMTRecord_").append(new SimpleDateFormat("dd-MM-yyyy-hh_mm_ss")
+            .format(new Date())).append(" .mp4").toString();
+
+            mediaRecorder.setOutputFile(videoURI);
+            mediaRecorder.setVideoSize(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+            mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.setVideoEncodingBitRate(512*1000);
+            mediaRecorder.setVideoFrameRate(30);
+
+            int rotation = getWindowManager().getDefaultDisplay().getRotation();
+            int orientation = ORIENTATIONS.get(rotation*90);
+            mediaRecorder.setOrientationHint(orientation);
+            mediaRecorder.prepare();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void recordScreen(){
+
+        if(mediaProjection == null){
+
+            startActivityForResult(mediaProjectionManager.createScreenCaptureIntent(), REQUEST_CODE);
+            return;
+        }
+        virtualDisplay = createVirtualDisplay();
+        mediaRecorder.start();
+
+    }
+
+    private VirtualDisplay createVirtualDisplay() {
+        return mediaProjection.createVirtualDisplay("ARScreen", DISPLAY_WIDTH, DISPLAY_HEIGHT, mScreenDensity,
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                mediaRecorder.getSurface(), null, null);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if( requestCode!= REQUEST_CODE){
+            Toast.makeText(this, "Unk error", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if( resultCode != RESULT_OK){
+            Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+            toggleButton.setChecked(false);
+            return;
+        }
+
+        mediaProjectionCallBack = new MediaProjectionCallBack();
+        mediaProjection = mediaProjectionManager.getMediaProjection(resultCode, data);
+        mediaProjection.registerCallback(mediaProjectionCallBack, null);
+        virtualDisplay = createVirtualDisplay();
+        mediaRecorder.start();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_PERMISSION:
+            {
+                if((grantResults.length > 0) && (grantResults[0] + grantResults[1] == PERMISSION_GRANTED)){
+                    toggleScreenShare(toggleButton);
+                }
+                else{
+                    toggleButton.setChecked(false);
+                    Snackbar.make(relativeLayout2, "Permissions", Snackbar.LENGTH_INDEFINITE)
+                            .setAction("ENABLE", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    ActivityCompat.requestPermissions(ARScreen.this,
+                                            new String[]{
+                                                    WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO},
+                                            REQUEST_PERMISSION);
+                                }
+
+                            }).show();
+                }
+                return;
+            }
+        }
+    }
 }
