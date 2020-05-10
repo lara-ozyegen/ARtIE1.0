64c6af2061ed5a82b8710d606d3b3f3ded59b63a
berksaltuk <saltuk.yilmaz@ug.bilkent.edu.tr>
2020-05-10 19:47:24 +0300

    Ladies and gentlemens, paint is fixed!

--- app/src/main/java/com/example/artie10/PaintActivity.java
+++ app/src/main/java/com/example/artie10/PaintActivity.java
@@ -3,8 +3,11 @@ package com.example.artie10;
         import androidx.appcompat.app.AppCompatActivity;
         import androidx.fragment.app.Fragment;
 
+        import android.content.Intent;
         import android.os.Bundle;
         import android.util.DisplayMetrics;
+        import android.view.View;
+        import android.widget.ImageButton;
 
         import com.example.artie10.PaintView;
 
@@ -12,15 +15,30 @@ public class PaintActivity extends AppCompatActivity {
 
     //proeprties
     private PaintView paintView;
-
+    ImageButton pencil;
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_paint );
 
+        pencil = (ImageButton) findViewById(R.id.pencil);
+        pencil.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick( View v ) {
+                        returnAR();
+                    }
+
+                    }
+        );
+
        paintView = ( PaintView ) findViewById( R.id.paintView );
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics( metrics );
         paintView.init( metrics );
+    }
+
+    public void returnAR(){
+        finish();
     }
 }
