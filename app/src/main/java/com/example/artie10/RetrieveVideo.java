d744af108475250900f08e452d11ae8401fdaea3
yrndurgun <63500510+yrndurgun@users.noreply.github.com>
2020-05-14 01:31:24 +0300

    RetrieveVideo class and layout. Will keep working on them.

---
+++ app/src/main/java/com/example/artie10/RetrieveVideo.java
@@ -0,0 +1,14 @@
+package com.example.artie10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RetrieveVideo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retrieve_video);
+    }
+}
