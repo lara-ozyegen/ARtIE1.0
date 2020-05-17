4ee99822246760b54774b17c9185ef732684bc5a
yrndurgun <63500510+yrndurgun@users.noreply.github.com>
2020-05-17 17:27:01 +0300

    play video.

---
+++ app/src/main/java/com/example/artie10/PlayVideo.java
@@ -0,0 +1,49 @@
+package com.example.artie10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+public class PlayVideo extends AppCompatActivity {
+
+    private VideoView videoView;
+    private ImageView play;
+    private TextView currentTimer;
+    private TextView durationTimer;
+    private ProgressBar videoProgress;
+    private Uri videoUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_video);
+        videoView = (VideoView) findViewById(R.id.videoView);
+        play = (ImageView) findViewById(R.id.playVideo);
+        currentTimer = (TextView) findViewById(R.id.currentTimer);
+        durationTimer = (TextView) findViewById(R.id.durationTimer);
+        videoProgress = (ProgressBar) findViewById(R.id.videoProgressBar);
+
+        playVideo();
+    }
+
+    public void playVideo(){
+        //this part was necessary to transfer some info from RetrieveVideo class
+        Bundle bundle = getIntent().getExtras();
+        String transferInfo = bundle.getString("transferInfo");
+
+        //transferInfo is download URL of the video in string form
+        videoUri = Uri.parse( transferInfo );
+
+        videoView.setVisibility( View.VISIBLE );
+        videoView.setVideoURI( videoUri );
+        videoView.requestFocus();
+        videoView.start();
+    }
+}
--- app/src/main/java/com/example/artie10/RetrieveVideo.java
+++ app/src/main/java/com/example/artie10/RetrieveVideo.java
@@ -3,6 +3,7 @@ package com.example.artie10;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -21,9 +22,9 @@ import java.io.File;
 import java.io.IOException;
 
 public class RetrieveVideo extends AppCompatActivity {
-    Button watchVideo;
-    EditText videoName;
-    StorageReference videoRef;
+    private Button watchVideo;
+    private EditText videoName;
+    private StorageReference videoRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +64,16 @@ public class RetrieveVideo extends AppCompatActivity {
                 Toast.makeText(RetrieveVideo.this, "offfff", Toast.LENGTH_SHORT).show();
             }
         });
+
+        //after retrieving  the video, navigate the user to UploadVideo page
+        Intent intent = new Intent (RetrieveVideo.this, PlayVideo.class );
+        //creating a bundle to transfer information to PlayVideo
+        Bundle bundle= new Bundle();
+        //to play a video from firebase, we need the reference to storage in string form
+        bundle.putString( "transferInfo", videoRef.getDownloadUrl() + "" );
+        //inserting the bundle into intent to be sent to PlayVideo
+        intent.putExtras( bundle );
+        startActivity( intent );
     }
 
         /*
