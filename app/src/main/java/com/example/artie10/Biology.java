ba9303ec6cf6d0f8c52d673090a551fa3c914a31
OykuErhan <oyku.erhan@ug.bilkent.edu.tr>
2020-05-18 16:21:51 +0300

    javadocs and small changes in biology and categor ies class

--- app/src/main/java/com/example/artie10/Biology.java
+++ app/src/main/java/com/example/artie10/Biology.java
@@ -13,33 +13,35 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
-
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author Öykü, Lara, Yaren, Sarper, Berk, Onur, Enis
+ * @version 1.0
+ * @date 15/04/2020
+ * This class shows models of biology
+ */
+
 public class Biology extends AppCompatActivity {
 
-    //CONSTANTS
+    //constant
     private static final int REQUEST_CODE = 45;
 
-    //VARIABLES
+    //variables
     Toolbar appbar;
-
     public ArrayAdapter<String> arrayAdapter;
     private List<String> myList;
     private ListView listView;
-
     private Button brainButton;
     private Button covidButton;
     private Button heartButton;
     private Button skullButton;
     private Button antibodyButton;
     private Button lungsButton;
-
     private String text;
 
     @Override
@@ -113,6 +115,9 @@ public class Biology extends AppCompatActivity {
         myList.add(( String) brainButton.getText());
     }
 
+    /**
+     * This method opens AR screen
+     */
     public void openAR() {
         if( MainActivity.getPath()){
             Intent intent = new Intent( this , ARScreenSession.class  );
@@ -125,27 +130,25 @@ public class Biology extends AppCompatActivity {
         }
     }
 
+    /**
+     * This method opens help screen
+     */
     public void openHelp(){
         Intent intent = new Intent( this, HelpScreen.class );
         startActivity( intent );
     }
 
+    /**
+     * This method returns home page
+     */
     public void openHome(){
         Intent intent = new Intent( this, MainActivity.class );
         startActivity( intent );
     }
 
-    protected void onActivityResult( int requestCode, int resultCode, Intent data ){
-        super.onActivityResult( requestCode, resultCode, data );
-        if( requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK ){
-            if( data != null ){
-                Uri uri = data.getData();
-                Toast.makeText( this, "Uri :" +uri, Toast.LENGTH_LONG ).show();
-                //Toast.makeText( this, "Path :" + uri.getPath(), Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-
+    /**
+     * This method opens search screen
+     */
     public void openSearch(){
         arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,myList );
         listView.setAdapter( arrayAdapter );
@@ -175,7 +178,6 @@ public class Biology extends AppCompatActivity {
 
         return super.onCreateOptionsMenu( menu );
     }
-
 
     @Override
     public boolean onOptionsItemSelected( MenuItem item ) {
